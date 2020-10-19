b62067de89cbf632bba5f8deb629c73ecfb8f2fa
Gustavo Henrique <gustavofmariano@gmail.com>
2020-10-19 17:36:55 -0300

    test: ensure LogControllerDecorator the same result of the controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -12,7 +12,7 @@ const makeController = (): Controller => {
       const httpResponse: HttpResponse = {
         statusCode: 200,
         body: {
-
+          name: 'Gustavo'
         }
       }
       return new Promise(resolve => resolve(httpResponse))
@@ -31,6 +31,25 @@ const makeSut = (): SutTypes => {
 
 describe('LogController decorator', () => {
   test('Should call controller handle ', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_mail@mail.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'Gustavo'
+      }
+    })
+  })
+
+  test('Should return the same result of the controller', async () => {
     const { sut, controllerStub } = makeSut()
     const handleSpy = jest.spyOn(controllerStub, 'handle')
     const httpRequest = {
