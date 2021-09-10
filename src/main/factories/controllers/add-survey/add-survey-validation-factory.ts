f3f519c40dc35cc7e80aa52203fe03332c55cf7a
Gustavo Henrique <gustavofmariano@gmail.com>
2021-09-10 18:23:59 -0300

    feat: add AddSurvey composition

---
+++ src/main/factories/controllers/add-survey/add-survey-validation-factory.ts
@@ -0,0 +1,10 @@
+import { RequireFieldValidation, ValidationComposite } from '../../../../validation/validators/index'
+import { Validation } from '../../../../presentation/protocols/validation'
+
+export const makeAddSurveyValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+  for (const field of ['question', 'answers']) {
+    validations.push(new RequireFieldValidation(field))
+  }
+  return new ValidationComposite(validations)
+}
