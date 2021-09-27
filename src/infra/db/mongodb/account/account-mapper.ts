a9bf9ed1d98ddfc96f0cd68204ef3b5e1422a093
Gustavo Henrique <gustavofmariano@gmail.com>
2021-09-27 01:23:55 -0300

    refactor: change AddSurvey to inset a date also

--- src/infra/db/mongodb/account/account-mapper.ts
+++
@@ -1,6 +0,0 @@
-import { AccountModel } from '../../../../domain/models/account'
-
-export const map = (account: any): AccountModel => {
-  const { _id, ...accountWithId } = account
-  return Object.assign({}, accountWithId, { id: _id })
-}
--- src/presentation/controllers/login/login/survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/login/login/survey/add-survey-controller.spec.ts
@@ -1,6 +1,7 @@
 import { Validation, HttpRequest, AddSurvey, AddSurveyModel } from './add-survey-controller-protocols'
 import { AddSurveyController } from './add-survey-controller'
 import { badRequest, noContent, serverError } from '../../../../helpers/http/http-helper'
+import MockDate from 'mockdate'
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
@@ -8,7 +9,8 @@ const makeFakeRequest = (): HttpRequest => ({
     answers: [{
       image: 'any_image',
       answer: 'any_answer'
-    }]
+    }],
+    date: new Date()
   }
 })
 
@@ -48,6 +50,14 @@ const makeSut = (): SutTypes => {
 }
 
 describe('AddSurvey Controller', () => {
+  beforeAll(async () => {
+    MockDate.set(new Date())
+  })
+
+  beforeAll(async () => {
+    MockDate.reset()
+  })
+
   test('Should call Validation with correct values', async () => {
     const { sut, validationStub } = makeSut()
     const validateSpy = jest.spyOn(validationStub, 'validate')
@@ -63,7 +73,7 @@ describe('AddSurvey Controller', () => {
     expect(httpResponse).toEqual(badRequest(new Error()))
   })
 
-  test('Should call AddSurvey', async () => {
+  test('Should call AddSurvey with correct values', async () => {
     const { sut, addSurveyStub } = makeSut()
     const addSpy = jest.spyOn(addSurveyStub, 'add')
     const httpRequest = makeFakeRequest()
@@ -71,7 +81,7 @@ describe('AddSurvey Controller', () => {
     expect(addSpy).toHaveBeenCalledWith(httpRequest.body)
   })
 
-  test('Should return 400 if AddSurvey throws', async () => {
+  test('Should return 500 if AddSurvey throws', async () => {
     const { sut, addSurveyStub } = makeSut()
     jest.spyOn(addSurveyStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const httpResponse = await sut.handle(makeFakeRequest())
