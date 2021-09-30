a90fa3f2cfbdb095996d6a7d3babb2f97316030e
Gustavo Henrique <gustavofmariano@gmail.com>
2021-09-30 19:27:29 -0300

    refactor: change models to use type alias instead of interfaces

--- src/validation/validators/validation-composite.spec.ts
+++ src/validation/validators/validation-composite.spec.ts
@@ -2,7 +2,7 @@ import { ValidationComposite } from './validation-composite'
 import { MissingParamError } from '@/presentation/erros'
 import { Validation } from '@/presentation/protocols'
 
-interface SutTypes {
+type SutTypes = {
   sut: ValidationComposite
   validationStubs: Validation[]
 }
